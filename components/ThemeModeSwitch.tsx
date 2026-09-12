e19efe3e8a4da26d6fc95c39cea47c8bfa4b46d7
你的名稱 <your-github-username@users.noreply.github.com>
2026-09-12 09:10:18 +0800

    chore: remove unused game components

--- components/ThemeModeSwitch.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import ThemeToggle from "@/components/ThemeToggle";
-
-/** 主題切換（日／夜／系統 文字段控），放在頁尾「關於我們」旁。 */
-export default function ThemeModeSwitch() {
-  return <ThemeToggle textOnly />;
-}
